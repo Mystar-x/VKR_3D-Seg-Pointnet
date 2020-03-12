95f39f231d640eefdd067692ddc259f1812ab0d6
Yansz <823972409@qq.com>
2020-03-12 12:52:56 +0300

    del repeated

--- Splatnet/exp/facade3d/dl_model_facade3d.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget http://maxwell.cs.umass.edu/splatnet-data/model_facade3d/snapshot_iter_16000.caffemodel
-wget http://maxwell.cs.umass.edu/splatnet-data/model_facade3d/net_deploy.prototxt
-wget http://maxwell.cs.umass.edu/splatnet-data/model_facade3d/net.prototxt
--- Splatnet/exp/shapenet3d/dl_model_shapenet3d.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget http://maxwell.cs.umass.edu/splatnet-data/model_shapenet3d.tar
-tar xf model_shapenet3d.tar
-mv model_shapenet3d/* .
-rm -rf model_shapenet3d
-rm model_shapenet3d.tar
